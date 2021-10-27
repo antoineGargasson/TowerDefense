b5f2051d04c45a7d5709f7e97545f94bf973eabe
antoineGargasson <92848889+antoineGargasson@users.noreply.github.com>
2021-10-27 10:00:41 +0200

    Destroying enemies

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -9,6 +9,8 @@ public class Tower : MonoBehaviour
     private float fireRate { get; set; } = 0.1f;
     private float currentFireRate { get; set; } = 0.0f;
 
+    private bool canShoot { get; set; } = true;
+
     private List<Enemy> enemies { get; set; } = null;
 
     private void Start()
@@ -18,7 +20,23 @@ public class Tower : MonoBehaviour
 
     private void Update()
     {
-        
+        if(!canShoot)
+        {
+            currentFireRate += Time.deltaTime;
+            if(currentFireRate >= fireRate)
+            {
+                currentFireRate = 0;
+                canShoot = true;
+            }
+            return;
+        }
+
+        if(enemies != null && enemies.Count > 0)
+        {
+            Destroy(enemies[0].gameObject);
+            enemies.RemoveAt(0);
+            canShoot = false;
+        }
     }
 
     public void Upgrade()
